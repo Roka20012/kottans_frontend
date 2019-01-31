05126049d90341573ed7f9ac04d2c2739fa96f75
Rostyk Broslavsky <broslavsky.rostislaw@gmail.com>
2019-01-31 15:00:49 +0200

    js oop exercise done

--- practical_task/a-tiny-JS-world/index.js
+++ practical_task/a-tiny-JS-world/index.js
@@ -1,49 +1,34 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: https://github.com/Roka20012/kottans_frontend/tree/master/practical_task/a-tiny-JS-world
-   Web app: https://roka20012.github.io/a-tiny-JS-world/
+   Code repository: https://roka20012.github.io/a-tiny-JS-world/
+   Web app: https://github.com/Roka20012/kottans_frontend/tree/master/practical_task/a-tiny-JS-world
    */
 'use strict';
 
-const dog = {
-  species: 'dog',
-  name: 'Toby',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'woof-woof!'
+class Creatures {
+    constructor(species, name, gender, legs, hands, saying) {
+      this.species = species,
+      this.name = name,
+      this.gender = gender,
+      this.legs = legs,
+      this.hands = hands,
+      this.saying = saying;
+    }
+}
+
+Creatures.prototype.sayHello = function(){
+  print(`Hi! I am ${this.species}; My name is <strong>${this.name}</strong>; My gender is <em>${this.gender}</em>;
+    \nI have ${this.legs} legs;I have ${this.hands} hands;I want to say ${this.saying};`);
 };
 
-const cat = {
-  species: 'cat',
-  name: 'Molly',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'meow'
-};
+const dog = new Creatures('dog', 'Toby', 'male', 4, 0, 'woof-woof!');
+const cat = new Creatures('cat', 'Molly', 'male', 4, 0, 'meow');
+const woman = new Creatures('human', 'Jenny', 'female', 2, 2, 'Hello Chris!');
+const man = new Creatures('human', 'John', 'male', 2, 2,' Hello Jenny!');
 
-const woman = {
-  species: 'human',
-  name: 'Jenny',
-  gender: 'female',
-  legs: 2,
-  hands: 2,
-  saying: 'Hello Chris!'
-};
+let allCreatures = [dog, cat, woman, man];
 
-const man = {
-  species: 'human',
-  name: 'John',
-  gender: 'male',
-  legs: 2,
-  hands: 2,
-  saying: 'Hello Jenny!'
-};
-
-let creatures = [dog, cat, woman, man];
-
-creatures.forEach(el => {
-  print(`Hi! I am ${el.species}; My name is <strong>${el.name}</strong>; My gender is <em>${el.gender}</em>;I have ${el.legs} legs;I have ${el.hands} hands;I want to say ${el.saying};`);
+allCreatures.forEach(el => {
+    el.sayHello();
 });
